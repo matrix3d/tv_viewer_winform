0668bd280f9d559a5b9ac3833caaf4eec78f5dc6
lizhi <405628079@qq.com>
2024-01-31 15:08:48 +0800

    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace webviewtest
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+			button1 = new Button();
+			menuStrip1 = new MenuStrip();
+			aToolStripMenuItem = new ToolStripMenuItem();
+			bToolStripMenuItem = new ToolStripMenuItem();
+			((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
+			menuStrip1.SuspendLayout();
+			SuspendLayout();
+			// 
+			// webView21
+			// 
+			webView21.AllowExternalDrop = true;
+			webView21.CreationProperties = null;
+			webView21.DefaultBackgroundColor = Color.White;
+			webView21.Dock = DockStyle.Fill;
+			webView21.Location = new Point(0, 58);
+			webView21.Margin = new Padding(3, 30, 3, 3);
+			webView21.Name = "webView21";
+			webView21.Size = new Size(800, 392);
+			webView21.TabIndex = 0;
+			webView21.ZoomFactor = 1D;
+			webView21.Click += webView21_Click;
+			// 
+			// button1
+			// 
+			button1.Dock = DockStyle.Top;
+			button1.Location = new Point(0, 25);
+			button1.Name = "button1";
+			button1.Size = new Size(800, 33);
+			button1.TabIndex = 1;
+			button1.Text = "reload";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
+			// menuStrip1
+			// 
+			menuStrip1.Items.AddRange(new ToolStripItem[] { aToolStripMenuItem, bToolStripMenuItem });
+			menuStrip1.Location = new Point(0, 0);
+			menuStrip1.Name = "menuStrip1";
+			menuStrip1.Size = new Size(800, 25);
+			menuStrip1.TabIndex = 2;
+			menuStrip1.Text = "menuStrip1";
+			// 
+			// aToolStripMenuItem
+			// 
+			aToolStripMenuItem.Name = "aToolStripMenuItem";
+			aToolStripMenuItem.Size = new Size(41, 21);
+			aToolStripMenuItem.Text = "urls";
+			// 
+			// bToolStripMenuItem
+			// 
+			bToolStripMenuItem.Name = "bToolStripMenuItem";
+			bToolStripMenuItem.Size = new Size(54, 21);
+			bToolStripMenuItem.Text = "about";
+			bToolStripMenuItem.Click += bToolStripMenuItem_Click;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 17F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(webView21);
+			Controls.Add(button1);
+			Controls.Add(menuStrip1);
+			MainMenuStrip = menuStrip1;
+			Name = "Form1";
+			Text = "tv 浏览器";
+			Load += Form1_Load;
+			((System.ComponentModel.ISupportInitialize)webView21).EndInit();
+			menuStrip1.ResumeLayout(false);
+			menuStrip1.PerformLayout();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+		private Button button1;
+		private MenuStrip menuStrip1;
+		private ToolStripMenuItem aToolStripMenuItem;
+		private ToolStripMenuItem bToolStripMenuItem;
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Security.Policy;
+
+namespace webviewtest
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			myinit();
+		}
+
+		void myinit()
+		{
+
+			button1.Click += (a, b) =>
+			{
+				webView21.CoreWebView2.Reload();// .Navigate("https://tv.cctv.com/live/cctv2/");
+												//MessageBox.Show("aaa");
+			};
+
+			string[] liveUrls = {
+			"https://tv.cctv.com/live/cctv1/",
+			"https://tv.cctv.com/live/cctv2/",
+			"https://tv.cctv.com/live/cctv3/",
+			"https://tv.cctv.com/live/cctv4/",
+			"https://tv.cctv.com/live/cctv5/",
+			"https://tv.cctv.com/live/cctv6/",
+			"https://tv.cctv.com/live/cctv7/",
+			"https://tv.cctv.com/live/cctv8/",
+			"https://tv.cctv.com/live/cctvjilu",
+			"https://tv.cctv.com/live/cctv10/",
+			"https://tv.cctv.com/live/cctv11/",
+			"https://tv.cctv.com/live/cctv12/",
+			"https://tv.cctv.com/live/cctv13/",
+			"https://tv.cctv.com/live/cctvchild",
+			"https://tv.cctv.com/live/cctv15/",
+			"https://tv.cctv.com/live/cctv16/",
+			"https://tv.cctv.com/live/cctv17/",
+			"https://tv.cctv.com/live/cctv5plus/",
+			"https://tv.cctv.com/live/cctveurope",
+			"https://tv.cctv.com/live/cctvamerica/",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002309",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002521",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002483",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002520",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002475",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002508",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002485",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002509",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002498",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002506",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002531",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002481",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002516",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002525",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002484",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002490",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002503",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002505",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002532",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002493",
+			"https://www.yangshipin.cn/#/tv/home?pid=600002513",
+			"https://www.bilibili.com/video/BV1NP411u7br/?spm_id_from=333.999.0.0&vd_source=14579d0fcae623fe6f711a6a0345a160",
+	};
+			webView21.Source = new Uri(liveUrls[6]);
+			for (var i=0;i< liveUrls.Length;i++)
+			{
+				var url = liveUrls[i];
+				var eitem = new ToolStripMenuItem();
+				eitem.Name = url;
+				eitem.Size = new Size(27, 21);
+				eitem.Text = url;
+				aToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { eitem });
+				eitem.Click += (a, b) =>
+				{
+
+					foreach (var item in aToolStripMenuItem.DropDownItems)
+					{
+						var item2 = item as ToolStripMenuItem;
+						if (item2 != null)
+						{
+							item2.Checked = false;
+						}
+					}
+					eitem.Checked = true;
+					webView21.CoreWebView2.Navigate(url);
+				};
+			}
+
+			webView21.NavigationStarting += (a, b) =>
+			{
+				//webView21.CoreWebView2.ExecuteScriptAsync(" document.querySelector('video').play();document.querySelector('video').requestFullscreen();");
+				webView21.CoreWebView2.ExecuteScriptAsync("" +
+					"var a= document.querySelector('video');" +
+					//"alert(a+document)"+
+					"window.addEventListener('load', function() {" +
+					"document.querySelector('video').requestFullscreen();" +
+					"});"
+					);
+				//webView21.CoreWebView2.ExecuteScriptAsync("fullscreen");
+				//.this.webView.InvokeScriptAsync("fullscreen", new string[] { }); .CoreWebView2.ExecuteScriptAsync($"alert('{b.Uri} is not safe, try an https link')");
+				//MessageBox.Show(b.Uri);
+			};
+		}
+
+		private void webView21_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void dToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void bToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
