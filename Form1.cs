83804c8adf5acd0b39f78bd0f885811fd8c5a6f8
lizhi <405628079@qq.com>
2024-02-02 16:07:39 +0800

    央视频提交

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Runtime.InteropServices;
 using System.Security.Policy;
 
 namespace webviewtest
 {
 	public partial class Form1 : Form
 	{
+		MyInteropObject ooo;
 		public Form1()
 		{
 			InitializeComponent();
@@ -14,6 +17,11 @@ namespace webviewtest
 
 		void myinit()
 		{
+			webView21.CoreWebView2InitializationCompleted += (a,b) => {
+				ooo = new MyInteropObject();
+				ooo.webView2 = webView21;
+				webView21.CoreWebView2.AddHostObjectToScript("Android",ooo );
+			};
 
 			button1.Click += (a, b) =>
 			{
@@ -42,27 +50,27 @@ namespace webviewtest
 			"https://tv.cctv.com/live/cctv5plus/",
 			"https://tv.cctv.com/live/cctveurope",
 			"https://tv.cctv.com/live/cctvamerica/",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002309",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002521",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002483",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002520",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002475",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002508",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002485",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002509",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002498",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002506",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002531",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002481",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002516",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002525",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002484",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002490",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002503",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002505",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002532",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002493",
-			"https://www.yangshipin.cn/#/tv/home?pid=600002513",
+			"https://www.yangshipin.cn/?a=1#/tv/home?pid=600002309",
+			"https://www.yangshipin.cn/?a=2#/tv/home?pid=600002521",
+			"https://www.yangshipin.cn/?a=3#/tv/home?pid=600002483",
+			"https://www.yangshipin.cn/?a=4#/tv/home?pid=600002520",
+			"https://www.yangshipin.cn/?a=5#/tv/home?pid=600002475",
+			"https://www.yangshipin.cn/?a=6#/tv/home?pid=600002508",
+			"https://www.yangshipin.cn/?a=7#/tv/home?pid=600002485",
+			"https://www.yangshipin.cn/?a=8#/tv/home?pid=600002509",
+			"https://www.yangshipin.cn/?a=9#/tv/home?pid=600002498",
+			"https://www.yangshipin.cn/?a=10#/tv/home?pid=600002506",
+			"https://www.yangshipin.cn/?a=11#/tv/home?pid=600002531",
+			"https://www.yangshipin.cn/?a=12#/tv/home?pid=600002481",
+			"https://www.yangshipin.cn/?a=13#/tv/home?pid=600002516",
+			"https://www.yangshipin.cn/?a=14#/tv/home?pid=600002525",
+			"https://www.yangshipin.cn/?a=15#/tv/home?pid=600002484",
+			"https://www.yangshipin.cn/?a=16#/tv/home?pid=600002490",
+			"https://www.yangshipin.cn/?a=17#/tv/home?pid=600002503",
+			"https://www.yangshipin.cn/?a=18#/tv/home?pid=600002505",
+			"https://www.yangshipin.cn/?a=19#/tv/home?pid=600002532",
+			"https://www.yangshipin.cn/?a=20#/tv/home?pid=600002493",
+			"https://www.yangshipin.cn/?a=21#/tv/home?pid=600002513",
 			"https://www.bilibili.com/video/BV1NP411u7br/?spm_id_from=333.999.0.0&vd_source=14579d0fcae623fe6f711a6a0345a160",
 	};
 			webView21.Source = new Uri(liveUrls[6]);
@@ -86,19 +94,43 @@ namespace webviewtest
 						}
 					}
 					eitem.Checked = true;
+					ooo.waitReload=false;
+					if (url.StartsWith("https://www.yangshipin.cn"))
+					{
+						ooo.waitReload = true;
+						//webView21.Reload();
+					}
 					webView21.CoreWebView2.Navigate(url);
 				};
 			}
-
-			webView21.NavigationStarting += (a, b) =>
+			
+			webView21.NavigationCompleted += (a, b) =>
 			{
 				//webView21.CoreWebView2.ExecuteScriptAsync(" document.querySelector('video').play();document.querySelector('video').requestFullscreen();");
-				webView21.CoreWebView2.ExecuteScriptAsync("" +
-					"var a= document.querySelector('video');" +
-					//"alert(a+document)"+
-					"window.addEventListener('load', function() {" +
-					"document.querySelector('video').requestFullscreen();" +
-					"});"
+				webView21.CoreWebView2.ExecuteScriptAsync(@"
+					function af(){ 
+                                         var fullscreenBtn = document.querySelector('#player_pagefullscreen_yes_player')||document.querySelector('.videoFull');
+                                         if(fullscreenBtn!=null){
+                                            //alert(fullscreenBtn)
+                                          fullscreenBtn.click();
+                                          document.querySelector('video').volume=1;
+                                         // alert(window.location.href+window.chrome.webview.hostObjects.Android.reload);
+											window.chrome.webview.hostObjects.Android.reload(""Hello from WebView!"")
+                                         }else{
+                                             setTimeout(
+                                                ()=>{ af();}
+                                            ,16); 
+                                         }
+                                     }
+                                af()
+
+					//var a= document.querySelector('video');
+					//alert(a+document)
+					//window.addEventListener('load', function() {
+						//alert(document.querySelector('video').requestFullscreen());	
+					//	document.querySelector('video').requestFullscreen();
+					//});
+					"
 					);
 				//webView21.CoreWebView2.ExecuteScriptAsync("fullscreen");
 				//.this.webView.InvokeScriptAsync("fullscreen", new string[] { }); .CoreWebView2.ExecuteScriptAsync($"alert('{b.Uri} is not safe, try an https link')");
@@ -129,6 +161,24 @@ namespace webviewtest
 		private void bToolStripMenuItem_Click(object sender, EventArgs e)
 		{
 
+		}
+	}
+}
+
+public class MyInteropObject
+{
+	public bool waitReload;
+	public WebView2 webView2;
+	// 在这里定义供 JavaScript 调用的方法
+	public void reload(string message)
+	{
+		if (waitReload)
+		{
+			//MessageBox.Show("C# method called from JavaScript with message: " + message);
+			//webView2.CoreWebView2.Reload();
+			waitReload = false;
+
+			//MessageBox.Show("C# method called from JavaScript with message:2 " + message);
 		}
 	}
 }
